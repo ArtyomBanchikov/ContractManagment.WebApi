a8542ef22bdec8b75b7739f7c209ab6939d49e2d
AngryKirill <mister.lolich@mail.ru>
2023-12-18 16:54:02 +0300

    Change DateOnly to DateTime in clients viewmodels

---
+++ ContractManagment.API/Converters/DateTimeTimestampConverter.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+
+namespace ContractManagment.API.Converters
+{
+    public class DateTimeToTimestampConverter : IValueConverter<DateTime, int>
+    {
+        public int Convert(DateTime sourceMember, ResolutionContext context)
+        {
+            double seconds = sourceMember.Subtract(new DateTime(1970, 1, 1)).TotalSeconds;
+            return System.Convert.ToInt32(seconds);
+        }
+    }
+    public class TimestampToDateTimeConverter : IValueConverter<int, DateTime>
+    {
+        public DateTime Convert(int sourceMember, ResolutionContext context)
+        {
+            DateTime dateTime = new DateTime(1970, 1, 1);
+            dateTime = dateTime.AddSeconds(sourceMember);
+            return dateTime;
+        }
+    }
+}
