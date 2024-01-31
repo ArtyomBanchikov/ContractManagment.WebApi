9ce49f1abc1b2564c363824046c5eca3d5cf8860
AngryKirill <mister.lolich@mail.ru>
2024-01-31 15:36:28 +0300

    add repositories for iptv clients

---
+++ ContractManagment.DAL/Repositories/ClientIPTV/ClientIPTVAddParamRepository.cs
@@ -0,0 +1,13 @@
+﻿using ContractManagment.DAL.EF;
+using ContractManagment.DAL.Entities.ClientIPTV;
+using ContractManagment.DAL.Repositories.Generic;
+
+namespace ContractManagment.DAL.Repositories.ClientIPTV
+{
+    public class ClientIPTVAddParamRepository : GenericBillingIPTVRepository<ClientIPTVAddParamEntity>
+    {
+        public ClientIPTVAddParamRepository(BillingIPTVContext context) : base(context)
+        {
+        }
+    }
+}
