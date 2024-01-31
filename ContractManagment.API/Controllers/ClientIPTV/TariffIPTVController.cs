e8fd89411421709d335f630a0e82f3e0dba5f476
AngryKirill <mister.lolich@mail.ru>
2024-01-31 18:29:09 +0300

    add iptv clients controllers

---
+++ ContractManagment.API/Controllers/ClientIPTV/TariffIPTVController.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using ContractManagment.API.Controllers.Generic;
+using ContractManagment.API.ViewModel.ClientIPTV;
+using ContractManagment.BLL.Interfaces.ClientIPTV;
+using ContractManagment.BLL.Models.ClientIPTV;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContractManagment.API.Controllers.ClientIPTV
+{
+    [ApiController]
+    [Route("[Controller]")]
+    [Authorize]
+    public class TariffIPTVController : GenericReadController<TariffIPTVModel, TariffIPTVViewModel>
+    {
+        public TariffIPTVController(ITariffIPTVService service, IMapper mapper) : base(service, mapper)
+        {
+        }
+    }
+}
