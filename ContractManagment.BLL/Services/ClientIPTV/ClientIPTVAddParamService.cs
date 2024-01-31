b758d18aee1ec040321f9e99f38ad0855e9b5116
AngryKirill <mister.lolich@mail.ru>
2024-01-31 17:53:08 +0300

    add IPTV clients services

---
+++ ContractManagment.BLL/Services/ClientIPTV/ClientIPTVAddParamService.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using ContractManagment.BLL.Interfaces.ClientIPTV;
+using ContractManagment.BLL.Models.ClientIPTV;
+using ContractManagment.BLL.Services.Generic;
+using ContractManagment.DAL.Entities.ClientIPTV;
+using ContractManagment.DAL.Interfaces.Generic;
+
+namespace ContractManagment.BLL.Services.ClientIPTV
+{
+    public class ClientIPTVAddParamService : GenericReadService<ClientIPTVAddParamModel, ClientIPTVAddParamEntity>, IClientIPTVAddParamService
+    {
+        public ClientIPTVAddParamService(IGenericReadRepository<ClientIPTVAddParamEntity> genericRepository, IMapper mapper) : base(genericRepository, mapper)
+        {
+        }
+    }
+}
---
+++ ContractManagment.BLL/Services/ClientIPTV/IPTVAddParamService.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using ContractManagment.BLL.Interfaces.ClientIPTV;
+using ContractManagment.BLL.Models.ClientIPTV;
+using ContractManagment.BLL.Services.Generic;
+using ContractManagment.DAL.Entities.ClientIPTV;
+using ContractManagment.DAL.Interfaces.Generic;
+
+namespace ContractManagment.BLL.Services.ClientIPTV
+{
+    public class IPTVAddParamService : GenericReadService<IPTVAddParamModel, IPTVAddParamEntity>, IIPTVAddParamService
+    {
+        public IPTVAddParamService(IGenericReadRepository<IPTVAddParamEntity> genericRepository, IMapper mapper) : base(genericRepository, mapper)
+        {
+        }
+    }
+}
