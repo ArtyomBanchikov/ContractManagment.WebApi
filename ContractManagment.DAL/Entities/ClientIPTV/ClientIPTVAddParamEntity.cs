9e77911d545a1a726cb7068d28b65482f0599d86
AngryKirill <mister.lolich@mail.ru>
2024-01-31 14:22:32 +0300

    Add entities for IPTV clients

---
+++ ContractManagment.DAL/Entities/ClientIPTV/ClientIPTVAddParamEntity.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContractManagment.DAL.Entities.ClientIPTV
+{
+    [Table("user_additional_params")]
+    public class ClientIPTVAddParamEntity
+    {
+        [Column("id"), Key]
+        public int Id { get; set; }
+
+        public int userid { get; set; }
+
+        [ForeignKey("userid")]
+        public ClientIPTVEntity? Client { get; set; }
+
+        public int paramid { get; set; }
+
+        [ForeignKey("paramid")]
+        public IPTVAddParamEntity? Parameter { get; set; }
+
+        [Column("value")]
+        public string Value { get; set; }
+    }
+}
---
+++ ContractManagment.DAL/Entities/ClientIPTV/IPTVAddParamEntity.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContractManagment.DAL.Entities.ClientIPTV
+{
+    [Table("uaddparams_desc")]
+    public class IPTVAddParamEntity
+    {
+        [Column("paramid"), Key]
+        public int Id { get; set; }
+
+        [Column("name")]
+        public string Name { get; set; }
+
+        [Column("display_name")]
+        public string DisplayName { get; set; }
+
+        [Column("visible")]
+        public int Visible { get; set; }
+    }
+}
