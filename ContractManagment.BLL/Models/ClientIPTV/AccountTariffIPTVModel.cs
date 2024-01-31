c04426fad02366ec7948cb95a3d6498820a7fba1
AngryKirill <mister.lolich@mail.ru>
2024-01-31 15:51:33 +0300

    add iptv clients models

---
+++ ContractManagment.BLL/Models/ClientIPTV/AccountTariffIPTVModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ContractManagment.BLL.Models.ClientIPTV
+{
+    internal class AccountTariffIPTVModel
+    {
+        public int Id { get; set; }
+        public int AccountId { get; set; }
+        public int TariffId { get; set; }
+        public TariffIPTVModel? Tariff { get; set; }
+        public int IsDeleted { get; set; }
+    }
+}
---
+++ ContractManagment.BLL/Models/ClientIPTV/ClientIPTVAddParamModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ContractManagment.BLL.Models.ClientIPTV
+{
+    public class ClientIPTVAddParamModel
+    {
+        public int Id { get; set; }
+        public int userid { get; set; }
+        public ClientIPTVModel? Client { get; set; }
+        public int paramid { get; set; }
+        public IPTVAddParamModel? Parameter { get; set; }
+        public string Value { get; set; }
+    }
+}
